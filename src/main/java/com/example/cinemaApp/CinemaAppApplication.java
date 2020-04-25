a20df6e9f88e6a8d29a6a46d076d8e81e2c33d4b
Velibor <veliborvasiljevic7@gmail.com>
2020-04-25 19:00:07 +0200

    init

---
+++ src/main/java/com/example/cinemaApp/CinemaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cinemaApp/CinemaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
