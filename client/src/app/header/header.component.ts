2fb03737e57b8060d7ef69eb5d9ef6a8c094779d
Velibor <veliborvasiljevic7@gmail.com>
2020-06-01 15:38:11 +0200

    header

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth/auth.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
+  constructor(public authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onLogout() {
+    this.authService.logout();
   }
-
 }
