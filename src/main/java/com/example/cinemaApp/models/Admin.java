cd3d4b1f05e7b6c5f83a90aa76903a54432a9fd8
Velibor <veliborvasiljevic7@gmail.com>
2020-06-20 15:30:53 +0200

    small fix

--- src/main/java/com/example/cinemaApp/models/Admin.java
+++ src/main/java/com/example/cinemaApp/models/Admin.java
@@ -1,21 +1,10 @@
 package com.example.cinemaApp.models;
 
-import java.io.Serializable;
-
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 @DiscriminatorValue("3")
 public class Admin extends User {
-
     private static final long serialVersionUID = 1L;
-
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
-    // private Integer id;
-
 }
